f4a55a5303bf35b28bea7e2fcdbf3fe3a5d84fda
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-05-21 13:59:15 +0100

    Updated with info from Giles

--- scholar.R
+++ scholar.R
@@ -1,18 +1,43 @@
 # Using the google scholar package
-#install.packages("scholar")
+assign(".lib.loc", "c:/Giles/R/Libraries", envir = environment(.libPaths))
+#install.packages("scholar",type='source',dependencies= TRUE,lib='c:/Giles/R/Libraries')
 
-library(scholar)
+library(tidyverse,lib='c:/Giles/R/Libraries')
+library(scholar,lib='c:/Giles/R/Libraries')
+
 
 # Roy's Google Scholar id
 # from https://scholar.google.co.uk/citations?user=z6jaMRcAAAAJ&hl=en
-roy_id <- "z6jaMRcAAAAJ&hl"
 
-roy_pubs <- get_publications(roy_id)
+mep_data <- read_csv("c:/Giles/Group stuff/Website/google_scholar_MEP.csv",
+                    na=c("(null)", "N/A","NA"))
 
-roy_pubs <- roy_pubs[order(roy_pubs$year),]
-roy_pubs <- roy_pubs[-1,] # Some random paper from 1977 credited to me!
 
-# Output dataframe has author, title, journal, year, citations etc.
-# also includes fields with unique journal and paper id codes which can
-# probably be dropped.
-roy_pubs <- subset(roy_pubs, select=-c(cid, pubid))
+scholar_ids<- mep_data$Scholar_ID
+
+all_pubs<-NULL
+
+for(ss in scholar_ids) {
+  get_publications(ss) %>%
+    rbind(all_pubs)  -> all_pubs   
+}
+
+all_pubs %>% 
+  
+  mutate(All_info=paste(year,title,author,journal,number,sep = ", "),
+         Title_lower=tolower(title)) %>% 
+  arrange(desc(All_info)) %>% 
+  distinct(pubid, .keep_all = TRUE) %>% 
+  distinct(Title_lower, .keep_all = TRUE) %>% 
+  filter(year!="1977") %>% #odd one for Roy  
+  dplyr::select(All_info)->all_pubs_trim  
+
+
+write_tsv(all_pubs_trim,"C:/Giles/Group stuff/Website/MEP_publications.txt")
+#write_csv((paste("C:/Giles/Group stuff/Website/Publications - Dist", ,".csv")))
+
+
+
+
+
+
